edc8bd20ae6d8cf24ed7887bfcb86ff3075acc18
anamarta26 <116230819+anamarta26@users.noreply.github.com>
2022-11-18 23:20:50 +0000

    Agora nos podemos incluir quaisquer heróis/vilões que o usuário queira digitar!

--- aula4_2022_11_16c.py
+++ aula4_2022_11_16c.py
@@ -1,8 +1,9 @@
 #1o. passo: importar a biblioteca sqlite3
 import sqlite3
 
+
 #PASSOS 2 e 3 VIRARÃO função conectar()
-def conectar():  
+def conectar():
   #2o. passo: Vamos estabelecer uma
   #conexão com o banco de dados
   conexao = sqlite3.connect("dc_universe.db")
@@ -10,4 +11,4 @@ def conectar():
   #3o. passo: criar um objeto do tipo cursor
   cursor = conexao.cursor()
 
-  return conexao, cursor 
+  return conexao, cursor
